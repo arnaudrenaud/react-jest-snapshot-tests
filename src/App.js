1f8bac00a61ca45a95b9bedf249bf60ec617382a
Arnaud Renaud <arnaud.renaud@gmail.com>
2018-12-12 14:53:08 +0100

    Fetch advice, load other advice on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    advice: '',
+  }
+
+  async componentDidMount() {
+    this.loadAdvice();
+  }
+
+  loadAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const { slip: { advice } } = await response.json();
+      this.setState({ advice });
+    } catch(error) {
+      this.setState({advice: ''});
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p className="Advice">{this.state.advice || '…'}</p>
+        <button
+          className="Advice-other-advice-button"
+          onClick={this.loadAdvice}
+        >
+          Show other advice
+        </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import fetchMock from 'fetch-mock';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  beforeEach(() => {
+    fetchMock.get(
+      'https://api.adviceslip.com/advice',
+      { slip : { advice: 'Always trust random advice.'} },
+      { repeat: 1 }
+    );
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+  });
+
+  describe('before advice fetched', () => {
+    it('renders correctly', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('after advice fetched', () => {
+    it('renders correctly', done => {
+      const wrapper = shallow(<App />);
+      setImmediate(() => {
+        expect(wrapper).toMatchSnapshot();
+        done();
+      });
+    });
+
+    describe('when button clicked', () => {
+      beforeEach(() => {
+        fetchMock.get(
+          'https://api.adviceslip.com/advice',
+          { slip : { advice: 'Other advice.'} },
+          { overwriteRoutes: false }
+        );
+      });
+
+      it('renders other random advice', done => {
+        const wrapper = shallow(<App />);
+        wrapper.find('.Advice-other-advice-button').simulate('click');
+        setImmediate(() => {
+          expect(wrapper).toMatchSnapshot();
+          done();
+        });
+      });
+    });
+  });
 });
